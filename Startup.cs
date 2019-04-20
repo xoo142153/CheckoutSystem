22325c9c756efc6a29bca8db2cc00f7f08a71814
Jehan Guiang <jehanguiang09@gmail.com>
2019-04-20 16:41:56 +0200

    disableSeeding

--- Startup.cs
+++ Startup.cs
@@ -83,7 +83,7 @@ namespace Cashier.API
             }
 
             //app.UseHttpsRedirection();
-            seeder.SeedUsers();
+             //seeder.SeedUsers();
             app.UseCors(x => x.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader());
             app.UseAuthentication();
             app.UseMvc();
